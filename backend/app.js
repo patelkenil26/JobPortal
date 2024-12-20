4926f7d5719d03e191ac3a391fd601bba4d33b6f
patelkenil26 <patelkenil2353@gmail.com>
2024-12-20 14:57:00 +0530

    cors update

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
-import express from "express";
 require('dotenv').config();
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { dbConnection } from "./database/dbConnection.js";
